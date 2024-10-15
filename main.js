0e2770615b7867f15ceb9ccb0b134069ee299c18
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-15 10:03:36 -0500

    change: isolate document body into variable for easy calling

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let DOC_BODY = document.body; // Abstract document body into variable for easy calling
+
 function createParagraph() {
-    var para = document.createElement('p')
-    para.innerHTML = "New Paragraph!!!"
-    document.body.append(para)
+    var para = document.createElement('p');
+    para.innerHTML = "New Paragraph!!!";
+
+    DOC_BODY.append(para);
 }
